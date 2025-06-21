f04591422b20b8b3566220ca7152411d180d8615
k5va <k5va@yandex.ru>
2025-06-21 20:56:29 +0200

    init

---
+++ orders-service/src/main/java/by/javaguru/orders/service/handler/OrderCommandsHandler.java
@@ -0,0 +1,30 @@
+package by.javaguru.orders.service.handler;
+
+import by.javaguru.core.dto.commands.ApproveOrderCommand;
+import by.javaguru.core.dto.commands.RejectOrderCommand;
+import by.javaguru.orders.service.OrderService;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics="${orders.commands.topic.name}")
+public class OrderCommandsHandler {
+
+    private final OrderService orderService;
+
+    public OrderCommandsHandler(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @KafkaHandler
+    public void handleCommand(@Payload ApproveOrderCommand approveOrderCommand) {
+        orderService.approveOrder(approveOrderCommand.getOrderId());
+    }
+
+    @KafkaHandler
+    public void handleCommand(@Payload RejectOrderCommand rejectOrderCommand) {
+        orderService.rejectOrder(rejectOrderCommand.getOrderId());
+    }
+}
